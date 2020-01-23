04d8ca0b1d05b594ded469a29733cb773bb588e9
mamadkamal <mkamal@microsoft.com>
2020-01-23 13:55:19 -0800

    Initial commit

---
+++ Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+ 
+    class Account
+    {
+        public int AccountNumber  { get; set; }
+        public String AccountNmae { get; set; }
+        public decimal Balance { get; set; }
+        public String  AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string EmailAddress { get; set; }
+
+
+    }
+}
